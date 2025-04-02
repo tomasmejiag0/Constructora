65ad03cca09354299c1cde668c28167fab4cd742
Tomás Mejía García <tomasmejiag2@gmail.com>
2025-04-02 12:09:22 -0500

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rcpvsbbndteopqfqxaeg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjcHZzYmJuZHRlb3BxZnF4YWVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1NDgwOTAsImV4cCI6MjA1OTEyNDA5MH0.zLS_6SSHKq8pxtezN72DjA6QH8Q5Fo23loBFMW2bj6s'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
